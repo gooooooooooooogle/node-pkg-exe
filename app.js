c1d856d896aeff734af4159aecb697cfdd253c3e
gooooooooooooogle <1185025336@qq.com>
2021-11-22 17:32:28 +0800

    Update

--- app.js
+++ app.js
@@ -1,31 +1,57 @@
 const xlsx = require('node-xlsx')
 const fs = require('fs')
 
-const workSheetsFromFile = xlsx.parse('./resource/data.xls');
 
-const dataSource = workSheetsFromFile[1].data;
+doTest();
 
-for (let index = 1; index < dataSource.length; index++) {
-  const row = dataSource[index];
-  if (row.length >= 7) {
-    const col = row[7] || '';
-    let a = col.substring(58, 60);
-    let b = col.substring(62, 64);
-    a = parseInt(a, 16)
-    b = parseInt(b, 16)
-    if ((a > 160) || (a < 80)) {
-      // console.log('信号A：', a);
-      writeFiles('信号A：' +  a + '\n')
-    }
-    if ((b > 160) || (b < 80)) {
-      // console.log('信号B：', b);
-      writeFiles('信号B：' + b + '\n')
-    }
-  }
+function doTest (){
+	const workSheetsFromFile = xlsx.parse('./resource/data.xls');
+
+	const dataSource = workSheetsFromFile[0].data;
+
+	delFiles('./result.xls');
+
+	let saveData = [];
+	let headData = ['表号', '信号A', '信号B'];
+
+	saveData.push(headData);
+
+	for (let index = 1; index < dataSource.length; index++) {
+	  const row = dataSource[index];
+	  if (row.length >= 7) {
+		const meterAddr = row[1];
+		const col = row[7];
+		if (col.length >= 74) {
+			let a = col.substring(58, 60);
+			let b = col.substring(62, 64);
+			let inta = parseInt(a, 16);
+			let intb = parseInt(b, 16);
+			let rowData = [];
+			rowData[0] = meterAddr;
+			rowData[1] = inta;
+			rowData[2] = intb;
+			
+			saveData.push(rowData);
+		}
+	  }
+	}
+
+	writeXls(saveData);
 }
 
-console.log('----------------处理结束！请查看当前目录下result.txt文件----------------')
+function delFiles(filePath) {
+	fs.unlink(filePath, () => {})
+}
 
-function writeFiles(info) {
-  fs.appendFile('./result.txt', info, () => {})
+function writeXls(datas) {
+	const options = {'!cols': [{ wch: 18 }, { wch: 10 }, { wch: 10 } ]};
+    let buffer = xlsx.build([
+        {
+            name:'sheet1',
+            data:datas   
+        }
+    ], options);
+	fs.writeFile('./result.xls', buffer, { 'flag': 'w+' }, (err) => {
+		if (err) return 
+	})
 }
